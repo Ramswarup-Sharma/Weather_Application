221a579b4b144240ff13adc590f0ac308240cc89
Ramswarup sharma <115331810+Ramswarup-Sharma@users.noreply.github.com>
2022-12-26 18:55:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ff6bb81acfmsh1200fe1451ca0c5p132db4jsncbddbfbd3214',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const getWeather = (city)=>{
+	cityName.innerHTML = city;
+	fetch('https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=' + city, options)
+	.then(response => response.json())
+	.then((response) => {
+		// cloud_pct.innerHTML = response.cloud_pct
+		temp.innerHTML = response.temp
+		temp2.innerHTML = response.temp
+		feels_like.innerHTML = response.feels_like
+		humidity.innerHTML = response.humidity
+		humidity2.innerHTML = response.humidity
+		min_temp.innerHTML = response.min_temp
+		max_temp.innerHTML = response.max_temp
+		wind_speed.innerHTML = response.wind_speed
+		wind_speed2.innerHTML = response.wind_speed
+		wind_degrees.innerHTML = response.wind_degrees
+		sunrise.innerHTML = response.sunrise
+		sunset.innerHTML = response.sunset
+	})
+	.catch(err => console.error(err));
+}
+
+submit.addEventListener('click', (e)=>{
+	e.preventDefault();
+	getWeather(city.value);
+});
+
+getWeather('Rajasthan');
